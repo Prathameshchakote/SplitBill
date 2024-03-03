98dea0b410a7bb59cd6b64080b9335f977e79c9e
Prathamesh Chakote <30796045+Prathameshchakote@users.noreply.github.com>
2024-03-03 13:37:46 -0800

    Initial Commit

---
+++ SplitBill/SplitBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBillApp.swift
+//  SplitBill
+//
+//  Created by Prathamesh on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
